f56935cad534469a4af7b5106f4782518ae6cc4f
Salomon Fayyazuddin <salomon.fayyazuddin@gmail.com>
2019-12-20 21:50:24 +0100

    Day 6 Part 1 in progress

--- Day6.py
+++ Day6.py
@@ -8,9 +8,8 @@ class Node:
 
 
 def Day6(filename):
-    with open(filename) as f:
-        lines = f.readlines()
-    
+    lines = open(filename, 'r').read().splitlines()
+
     nodeList = []
 
     for data in lines:
@@ -26,31 +25,16 @@ def Day6(filename):
             node1 = Node(names[0])
             nodeList.append(node1)
 
-        if names[1] not in nodeList:
-            node2 = Node(names[1])
-            node2.setPrevNode(node1)
-            nodeList.append(node2)
+        node2 = Node(names[1])
+        node2.setPrevNode(node1)
+        nodeList.append(node2)
 
     totalOrbits = 0
 
     for item in nodeList:
-        if item.name == "K":
-            example = item
-    
-    iteration = 0
-    while example.prev is not None:
-        print("iteration nr " + str(iteration))
-        print("Name: " + str(example.name))
-        example = example.prev
-
-    for item in nodeList:
-        oldTotal = totalOrbits
-        print("Adding orbits for " + str(item.name))
         while item.prev != None:  
             totalOrbits += 1
             item = item.prev
-        nrOrbitsAdded = totalOrbits - oldTotal
-        print(str(nrOrbitsAdded) + " orbits added")
     return totalOrbits
 
 print(Day6("data.txt"))
